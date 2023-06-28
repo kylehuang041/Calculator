043045260f73ba19c2716cd51bd0cca2a343fdd3
kylehuang041 <kylehuang041@gmail.com>
2023-06-28 15:50:44 -0700

    recovered postfix Calculation.java! I was using the old one the whole time!

--- src/Calculation.java
+++ src/Calculation.java
@@ -1,72 +1,125 @@
+import java.util.ArrayList;
 import java.util.Stack;
 import org.apache.commons.math3.special.Gamma;
 
 class Calculation {
-	private static Stack<Double> operands = new Stack<>();
-	private static Stack<Character> operators = new Stack<>();
 
-	public static double solve(String str) {
+	public static String solve(String str) {
+		ArrayList<String> strArr = convertToPostFix(str);
+		return calculatePostFix(strArr);
+	}
+
+	private static ArrayList<String> convertToPostFix(String str) {
 		String[] parts = str.split("\\s+");
+		Stack<Character> tokens = new Stack<>();
+		ArrayList<String> postfix = new ArrayList<>();
 		int i = 0;
+
 		// traverse through expression
 		while (i < parts.length) {
 			// if factorial number
 			if (parts[i].contains("!")) {
 				double operand = Double.parseDouble(parts[i].substring(0, parts[i].length() - 1));
-				operands.push(factorial(operand));
+				postfix.add(String.valueOf(factorial(operand)));
 				// if current string contains PI, then add to operand
-			} else if (parts[i].charAt(0) == '\u03c0') {
-				operands.push(Math.PI);
-				// oif current string is a square root, then add to operand
-			} else if (parts[i].charAt(0) == '\u221A') {
-				operands.push(Math.sqrt(operands.pop()));
-				// if current string is ')' then solve for the parenthesis expression and add to operands
-			} else if (parts[i].equals(")")) {
-				while (operators.peek() != '(') {
-					char operator = operators.pop();
-					double operand2 = operands.pop();
-					double operand1 = operands.pop();
-					calculate(operand1, operand2, operator);
+			} else if (parts[i].contains("\u03c0")) {
+				if (parts[i].charAt(0) == '\u03c0') {
+					postfix.add(String.valueOf(Math.PI));
+				} else {
+					double num = Double.parseDouble(parts[i].substring(0, parts[i].length() - 1));
+					postfix.add(String.valueOf(num * Math.PI));
 				}
-				operators.pop();
+				// if current string is a square root, then add to operand
+			} else if (parts[i].charAt(0) == '\u221A') {
+				postfix.add(String.valueOf(Math.sqrt(Double.parseDouble(parts[i + 1]))));
+				i++; // Skip the next token as it has been processed as a square root
+				// if current string is ')' then solve for the parenthesis expression and add to
+				// operands
+			} else if (parts[i].equals("(")) {
+				tokens.push('(');
+			} else if (parts[i].equals(")")) {
+				while (!tokens.empty() && tokens.peek() != '(')
+					postfix.add(String.valueOf(tokens.pop()));
+				tokens.pop();
 				// if current string is an integer, add to number to operands
 			} else if (isNumber(parts[i])) {
-				operands.push(Double.parseDouble(parts[i]));
+				postfix.add(parts[i]);
 				if (i + 1 < parts.length && parts[i + 1].equals("(")) {
-					operators.push('*');
+					tokens.push('*');
 				}
-			// push operators
-			} else {
-				operators.push(parts[i].charAt(0));
+				// push operators
+			} else if (isOperator(parts[i].charAt(0))) {
+				while (!tokens.empty() && tokens.peek() != '(' && isOperatorLessEqualTo(parts[i].charAt(0), tokens.peek())) {
+					postfix.add(String.valueOf(tokens.pop()));
+				}
+				tokens.push(parts[i].charAt(0));
 			}
 			++i;
 		}
 
-		// calculate the rest of the operands
-		while (operators.size() > 0) {
-			char operator = operators.pop();
-			double operand2 = operands.pop();
-			double operand1 = operands.pop();
-			calculate(operand1, operand2, operator);
-		}
-
-		return operands.pop();
+		while (!tokens.empty())
+			postfix.add(String.valueOf(tokens.pop()));
+		return postfix;
 	}
 
-	private static void calculate(double operand1, double operand2, char operator) {
-		if (operator == '*') {
-			operands.push(operand1 * operand2);
-		} else if (operator == '/') {
-			operands.push(operand1 / operand2);
-		} else if (operator == '+') {
-			operands.push(operand1 + operand2);
-		} else if (operator == '-') {
-			operands.push(operand1 - operand2);
-		} else if (operator == '%') {
-			operands.push(operand1 % operand2);
-		} else if (operator == '^') {
-			operands.push(Math.pow(operand1, operand2));
+	private static String calculatePostFix(ArrayList<String> postfix) {
+		Stack<String> stack = new Stack<>();
+		int i = 0;
+
+		while (i < postfix.size()) {
+			if (isOperator(postfix.get(i).charAt(0))) {
+				char operator = postfix.get(i).charAt(0);
+				double op2 = Double.parseDouble(stack.pop());
+				double op1 = Double.parseDouble(stack.pop());
+				double result = calculate(op1, op2, operator);
+				stack.push(String.valueOf(result));
+			} else {
+				stack.push(postfix.get(i));
+			}
+			++i;
 		}
+
+		return stack.pop();
+	}
+
+	private static boolean isOperator(char c) {
+		return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '%';
+	}
+
+	private static boolean isOperatorLessEqualTo(char x, char y) {
+		// if x < y in terms of operator precedence order, return true
+		if ((x == '+' || x == '-') && (y == '*' || y == '/' || y == '%' || y == '^'))
+			return true;
+
+		// if x == y in terms of operator high precedence order, return true
+		else if ((x == '*' || x == '/' || x == '%' || x == '^') && (y == '*' || y == '/'
+				|| y == '%' || y == '^'))
+			return true;
+
+		// if x == y in terms of operator low precedence order, return true
+		else if ((x == '+' || x == '-') && (y == '+' || y == '-'))
+			return true;
+
+		// otherwise, return false
+		else
+			return false;
+	}
+
+	private static double calculate(double operand1, double operand2, char operator) {
+		if (operator == '*') {
+			return operand1 * operand2;
+		} else if (operator == '/') {
+			return operand1 / operand2;
+		} else if (operator == '+') {
+			return operand1 + operand2;
+		} else if (operator == '-') {
+			return operand1 - operand2;
+		} else if (operator == '%') {
+			return operand1 % operand2;
+		} else if (operator == '^') {
+			return Math.pow(operand1, operand2);
+		}
+		throw new IllegalArgumentException();
 	}
 
 	public static boolean isNumber(String str) {
@@ -76,13 +129,13 @@ class Calculation {
 	/**
 	 * PRE: requires a starting integer value
 	 * POST: return the factorial of the value
-	 * 
-	 * @param num starting value or value
+	 *
+	 * @param num        starting value or value
+	 * @param isNegative flag indicating if the value is negative
 	 * @return result -> factorial of the value
 	 */
 	private static double factorial(double num) {
 		boolean isNegative = num < 0 ? true : false;
-		num = Math.abs(num);
-		return isNegative ? -Gamma.gamma(num + 1) : Gamma.gamma(num + 1);
+		return isNegative ? -1 * Gamma.gamma(num) : Gamma.gamma(num);
 	}
 }
