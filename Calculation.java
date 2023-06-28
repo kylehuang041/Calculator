436badf334f08e6d15e1b7039e41db43079aa1db
kylehuang041 <kylehuang041@gmail.com>
2023-06-28 01:07:47 -0700

    Squashed commit of the following:
    commit 01c360f56e8bf7e7d0b7b2486c918ad95d44fd4b
    Author: kylehuang041 <kylehuang041@gmail.com>
    Date:   Wed Jun 28 01:00:44 2023 -0700
    
        finished with stack without order of precedence
    
    commit 8eab7d484e43be93ac919e83a09abca681b40448
    Author: kylehuang041 <kylehuang041@gmail.com>
    Date:   Wed Jun 28 00:13:19 2023 -0700
    
        fix parantheses
    
    commit 0e71a96db9e57a48d0ac93ca710a852cc5bbcb97
    Author: kylehuang041 <kylehuang041@gmail.com>
    Date:   Tue Jun 27 23:57:35 2023 -0700
    
        save
    
    commit 6f169759ad41a113f0ecfc59b25ecfbedc4b0e0c
    Author: kylehuang041 <kylehuang041@gmail.com>
    Date:   Tue Jun 27 21:38:55 2023 -0700
    
        save

---
+++ Calculation.java
@@ -0,0 +1,108 @@
+import java.util.Stack;
+
+class Calculation {
+	private static Stack<Double> operands = new Stack<>();
+	private static Stack<Character> operators = new Stack<>();
+
+	public static double solve(String str) {
+		String[] parts = str.split("\\s+");
+		int i = 0;
+		// traverse through expression
+		while (i < parts.length) {
+			// if factorial number
+			if (parts[i].contains("!")) {
+				double operand = Double.parseDouble(parts[i].substring(0, parts[i].length() - 1));
+				boolean isNegative = operand < 0 ? true : false;
+				operands.push(factorial(operand, isNegative));
+				// if current string contains PI, then add to operand
+			} else if (parts[i].charAt(0) == '\u03c0') {
+				operands.push(Math.PI);
+				// oif current string is a square root, then add to operand
+			} else if (parts[i].charAt(0) == '\u221A') {
+				operands.push(Math.sqrt(operands.pop()));
+				// if current string is ')' then solve for the parenthesis expression and add to operands
+			} else if (parts[i].equals(")")) {
+				while (operators.peek() != '(') {
+					char operator = operators.pop();
+					double operand2 = operands.pop();
+					double operand1 = operands.pop();
+					calculate(operand1, operand2, operator);
+				}
+				operators.pop();
+				// if current string is an integer, add to number to operands
+			} else if (isNumber(parts[i])) {
+				operands.push(Double.parseDouble(parts[i]));
+				if (i + 1 < parts.length && parts[i + 1].equals("(")) {
+					operators.push('*');
+				}
+			// push operators
+			} else {
+				operators.push(parts[i].charAt(0));
+			}
+			++i;
+		}
+
+		// calculate the rest of the operands
+		while (operators.size() > 0) {
+			char operator = operators.pop();
+			double operand2 = operands.pop();
+			double operand1 = operands.pop();
+			calculate(operand1, operand2, operator);
+		}
+
+		return operands.pop();
+	}
+
+	private static void calculate(double operand1, double operand2, char operator) {
+		if (operator == '*') {
+			operands.push(operand1 * operand2);
+		} else if (operator == '/') {
+			operands.push(operand1 / operand2);
+		} else if (operator == '+') {
+			operands.push(operand1 + operand2);
+		} else if (operator == '-') {
+			operands.push(operand1 - operand2);
+		} else if (operator == '%') {
+			operands.push(operand1 % operand2);
+		} else if (operator == '^') {
+			operands.push(Math.pow(operand1, operand2));
+		}
+	}
+
+	public static boolean isNumber(String str) {
+		return str.matches("-?\\d+(\\.\\d+)?");
+	}
+
+	/**
+	 * PRE: requires a base and exponent
+	 * POST: return the result of a exponential expression
+	 * 
+	 * @param base     base number
+	 * @param exponent exponent number
+	 * @return result -> result of a exponential expression
+	 */
+	private static double power(double base, double exponent) {
+		double res = 1;
+		for (int i = 1; i <= Math.abs(exponent); i++) {
+			res *= base;
+		}
+		return (exponent > 0) ? res : 1 / res;
+	}
+
+	/**
+	 * PRE: requires a starting integer value
+	 * POST: return the factorial of the value
+	 * 
+	 * @param num starting value or value
+	 * @return result -> factorial of the value
+	 */
+	private static double factorial(double num, boolean isNegative) {
+		num = Math.round(num);
+		if (num < 0)
+			isNegative = true;
+		if (num == 0)
+			return (isNegative) ? -1 : 1;
+		return (!isNegative) ? num * factorial(num - 1, isNegative)
+				: num * factorial(num + 1, isNegative);
+	}
+}
--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -6,245 +7,182 @@ import java.awt.event.*;
 /**
  * Java GUI Calculator: calculates from left to right with no order of
  * operations
+ * 
  * @author Kyle Huang
  * @version 5.8
  * @since 12/23/2021
  */
 public class Calculator extends JFrame {
 
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-    }
+	public static void main(String[] args) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new Calculator();
+			}
+		});
+	}
 
-    private JFrame frame;
-    private JTextArea text;
-    private JButton zero, one, two, three, four, five, six, seven, eight,
-            nine, add, subtract, multiply, divide, clear, delete, equal,
-            negative, power, factorial, decimal, pi;
+	private JFrame frame;
+	private JTextArea text;
+	private JButton zero, one, two, three, four, five, six, seven, eight,
+			nine, add, subtract, multiply, divide, clear, delete, equal,
+			negative, power, factorial, decimal, pi, modulo, sqrt, lparan, rparan;
 
-    /**
-     * PRE: requires coordinates and name for frame,
-     * coordinates and size for text area, and
-     * size for buttons.
-     * POST: creates the frame, textarea, and buttons.
-     */
-    public Calculator() {
-        createFrame(330, 440, "Calculator");
-        createTextArea(10, 10, 315, 40);
-        createButtons(60, 40);
-    }
+	/**
+	 * PRE: requires coordinates and name for frame,
+	 * coordinates and size for text area, and
+	 * size for buttons.
+	 * POST: creates the frame, textarea, and buttons.
+	 */
+	public Calculator() {
+		createFrame(330, 500, "Calculator");
+		createTextArea(10, 10, 315, 40);
+		createButtons(60, 40);
+	}
 
-    /**
-     * PRE: require string from textarea block
-     * POST: calculates the expression from the string
-     * @param equ equation or expression
-     * @return result of expression
-     */
-    private double calculate(String equ) {
-        Scanner reader = new Scanner(equ);
-        double res = 0;
+	/**
+	 * PRE: requires size and name
+	 * POST: set the size and name of frame
+	 * 
+	 * @param width  width of frame
+	 * @param height height of frame
+	 * @param name   name of frame
+	 */
+	private void createFrame(int width, int height, String name) {
+		this.frame = new JFrame(name);
+		this.frame.setSize(width, height);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setResizable(false);
+		this.frame.setLayout(null);
+	}
 
-        try {
-            while (reader.hasNext()) {
-                String tmp = reader.next();
-                if (tmp.equalsIgnoreCase("*"))
-                    res *= calcHelper(reader.next());
-                else if (tmp.equalsIgnoreCase("/"))
-                    res /= calcHelper(reader.next());
-                else if (tmp.equalsIgnoreCase("+"))
-                    res += calcHelper(reader.next());
-                else if (tmp.equalsIgnoreCase("-"))
-                    res -= calcHelper(reader.next());
-                else if (tmp.contains("!") || tmp.contains("^")) res += calcHelper(tmp);
-                else if (tmp.contains("\u03c0")) res += calcHelper(tmp);
-                else res += Double.parseDouble(tmp);
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Error: Number split");
-        } catch (NumberFormatException e) {
-            System.out.println("Error: Number formating");
-        } catch (Exception e) {
-            System.out.println("Error: Finish the expression");
-        } finally {
-            reader.close();
-        }
+	/**
+	 * PRE: requires coordinates and size
+	 * POST: set the coordinates and size of text area
+	 * 
+	 * @param x starting x-coordinate of text area
+	 * @param y starting y-coordinate of text area
+	 * @param w width of text area
+	 * @param h height of text area
+	 */
+	private void createTextArea(int x, int y, int w, int h) {
+		this.text = new JTextArea();
+		this.text.setLocation(x, y);
+		this.text.setSize(w - (2 * x), h);
+		Font font = new Font("SansSerif", Font.BOLD, 22);
+		this.text.setFont(font);
+		this.text.setEditable(false);
+		this.frame.add(text);
+	}
 
-        return res;
-    }
+	private void createButtonsHelper() {
+		// create buttons
+		zero = new JButton("0");
+		one = new JButton("1");
+		two = new JButton("2");
+		three = new JButton("3");
+		four = new JButton("4");
+		five = new JButton("5");
+		six = new JButton("6");
+		seven = new JButton("7");
+		eight = new JButton("8");
+		nine = new JButton("9");
+		add = new JButton("+");
+		subtract = new JButton("-");
+		multiply = new JButton("*");
+		divide = new JButton("/");
+		clear = new JButton("C");
+		delete = new JButton("\u232B");
+		equal = new JButton("=");
+		negative = new JButton("(-)");
+		power = new JButton("^");
+		factorial = new JButton("!");
+		decimal = new JButton(".");
+		pi = new JButton("\u03c0");
+		modulo = new JButton("%");
+		sqrt = new JButton("\u221A");
+		lparan = new JButton("(");
+		rparan = new JButton(")");
+	}
 
-    /**
-     * PRE: requires a String as input
-     * POST: If the String contains "^", it would calculate the power.
-     * If the String contains "!", it would calculate the factorial.
-     * Otherwise, return the String.
-     * @param tmp temporary String
-     * @return finished calculation of power or factorial. Otherwise,
-     *         the String itself from the beginning if it does not
-     *         contain "^" (power) or "!" (factorial).
-     */
-    private double calcHelper(String tmp) {
-        if (tmp.contains("^")) {
-            String[] parts = tmp.replace("^", " ").split(" ");
-            return power(Double.parseDouble(parts[0]),
-                    Double.parseDouble(parts[1]));
-        } else if (tmp.contains("!")) {
-            return factorial(Double.parseDouble(tmp.replace("!", "")), false);
-        } else if (tmp.contains("\u03c0")) {
-            final double PI = 3.14159265359;
-            if (tmp.equalsIgnoreCase("\u03c0")) return PI;
-            String[] parts = tmp.split("\u03c0");
-            return Double.parseDouble(parts[0])* PI;
-        }
-        return Double.parseDouble(tmp);
-    }
+	/**
+	 * PRE: requires size of buttons
+	 * POST: creates the buttons and its functionalities
+	 * 
+	 * @param w width of buttons
+	 * @param h height of buttons
+	 */
+	private void createButtons(int w, int h) {
+		createButtonsHelper();
 
-    /**
-     * PRE: requires size and name
-     * POST: set the size and name of frame
-     * @param width  width of frame
-     * @param height height of frame
-     * @param name   name of frame
-     */
-    private void createFrame(int width, int height, String name) {
-        this.frame = new JFrame(name);
-        this.frame.setSize(width, height);
-        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setResizable(false);
-        this.frame.setLayout(null);
-    }
+		int gap = (this.frame.getWidth() - (4 * w)) / 5, startX = 10, startY = 60;
+		ArrayList<ArrayList<JButton>> list = new ArrayList<>();
 
-    /**
-     * PRE: requires coordinates and size
-     * POST: set the coordinates and size of text area
-     * @param x starting x-coordinate of text area
-     * @param y starting y-coordinate of text area
-     * @param w width of text area
-     * @param h height of text area
-     */
-    private void createTextArea(int x, int y, int w, int h) {
-        this.text = new JTextArea();
-        this.text.setLocation(x, y);
-        this.text.setSize(w - (2 * x), h);
-        Font font = new Font("SansSerif", Font.BOLD, 22);
-        this.text.setFont(font);
-        this.text.setEditable(false);
-        this.frame.add(text);
-    }
+		// add buttons to arraylist
+		list.add(new ArrayList<>(Arrays.asList(modulo, factorial, clear, delete)));
+				list.add(new ArrayList<>(Arrays.asList(pi, power, sqrt, divide)));
+		list.add(new ArrayList<>(Arrays.asList(seven, eight, nine, multiply)));
+		list.add(new ArrayList<>(Arrays.asList(four, five, six, subtract)));
+		list.add(new ArrayList<>(Arrays.asList(one, two, three, add)));
+		list.add(new ArrayList<>(Arrays.asList(negative, zero, decimal, lparan)));
+		list.add(new ArrayList<>(Arrays.asList(rparan, equal)));
 
-    /**
-     * PRE: requires size of buttons
-     * POST: creates the buttons and its functionalities
-     * @param w width of buttons
-     * @param h height of buttons
-     */
-    private void createButtons(int w, int h) {
-        // create buttons
-        zero = new JButton("0");
-        one = new JButton("1");
-        two = new JButton("2");
-        three = new JButton("3");
-        four = new JButton("4");
-        five = new JButton("5");
-        six = new JButton("6");
-        seven = new JButton("7");
-        eight = new JButton("8");
-        nine = new JButton("9");
-        add = new JButton("+");
-        subtract = new JButton("-");
-        multiply = new JButton("*");
-        divide = new JButton("/");
-        clear = new JButton("C");
-        delete = new JButton("\u232B");
-        equal = new JButton("=");
-        negative = new JButton("(-)");
-        power = new JButton("^");
-        factorial = new JButton("!");
-        decimal = new JButton(".");
-        pi = new JButton("\u03c0");
+		// place and add buttons onto screen
+		for (int i = 0; i < list.size(); i++) {
+			ArrayList<JButton> temp = list.get(i);
+			for (int j = 0; j < list.get(i).size(); j++) {
+				JButton tempBtn = temp.get(j);
+				Font font = new Font("SansSerif", Font.BOLD, 16);
+				tempBtn.setFont(font);
+				tempBtn.setBounds(startX + (w * j) + (gap * j),
+						startY + (i * h) + (i * gap), w, h);
 
-        // variables
-        int gap = (this.frame.getWidth() - (4 * w)) / 5, startX = 10,
-                startY = 60;
-        ArrayList<ArrayList<JButton>> list = new ArrayList<>();
-
-        // add buttons to arraylist
-        list.add(new ArrayList<>(Arrays.asList(one, two, three, delete)));
-        list.add(new ArrayList<>(Arrays.asList(four, five, six, clear)));
-        list.add(new ArrayList<>(Arrays.asList(seven, eight, nine, add)));
-        list.add(new ArrayList<>(Arrays.asList(subtract, zero, multiply,
-                divide)));
-        list.add(new ArrayList<>(Arrays.asList(negative, power, factorial,
-                equal)));
-        list.add(new ArrayList<>(Arrays.asList(decimal, pi)));
-
-        // place and add buttons onto screen
-        for (int i = 0; i < list.size(); i++) {
-            ArrayList<JButton> temp = list.get(i);
-            for (int j = 0; j < list.get(i).size(); j++) {
-                JButton tempBtn = temp.get(j);
-                Font font = new Font("SansSerif", Font.BOLD, 16);
-                tempBtn.setFont(font);
-                tempBtn.setBounds(startX + (w * j) + (gap * j),
-                        startY + (i * h) + (i * gap), w, h);
-
-                // create functionalities
-                tempBtn.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        String BtnText = tempBtn.getText();
-                        if (BtnText.equalsIgnoreCase("="))
-                            text.setText(String.valueOf(calculate(
-                            text.getText())));
-                        else if (BtnText.equalsIgnoreCase("C"))
-                            text.setText("");
-                        else if (BtnText.equalsIgnoreCase("\u232B")) {
-                            if (text.getText().length() > 0)
-                                text.setText(text.getText().substring(0,
-                                text.getText().length() - 1));
-                        } else if (BtnText.equalsIgnoreCase("+")
-                            || BtnText.equalsIgnoreCase("-")
-                            || BtnText.equalsIgnoreCase("*")
-                            || BtnText.equalsIgnoreCase("/"))
-                                text.append(" " + BtnText + " ");
-                        else if (BtnText.equalsIgnoreCase("(-)"))
-                            text.append("-");
-                        else
-                            text.append(BtnText);
-                    }
-                });
-                this.frame.add(temp.get(j));
-            }
-        }
-        this.frame.setVisible(true);
-    }
-
-    /**
-     * PRE: requires a base and exponent
-     * POST: return the result of a exponential expression
-     * @param base base number
-     * @param exponent exponent number
-     * @return result -> result of a exponential expression
-     */
-    private static double power(double base, double exponent) {
-        double res = 1;
-        for (int i = 1; i <= Math.abs(exponent); i++) {
-            res *= base;
-        }
-        return (exponent > 0) ? res : 1 / res;
-    }
-
-    /**
-     * PRE: requires a starting integer value
-     * POST: return the factorial of the value
-     * @param num starting value or value
-     * @return result -> factorial of the value
-     */
-    private static double factorial(double num, boolean isNegative) {
-        num = Math.round(num);
-        if (num < 0)
-            isNegative = true;
-        if (num == 0)
-            return (isNegative) ? -1 : 1;
-        return (!isNegative) ? num * factorial(num - 1, isNegative)
-            : num * factorial(num + 1, isNegative);
-    }
+				// create functionalities
+				tempBtn.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						String BtnText = tempBtn.getText();
+						// result
+						if (BtnText.equalsIgnoreCase("=")) {
+							text.setText(String.valueOf(Calculation.solve(text.getText())));
+							// Clear
+						} else if (BtnText.equalsIgnoreCase("C")) {
+							text.setText("");
+							// Delete
+						} else if (BtnText.equalsIgnoreCase("\u232B")) {
+							if (text.getText().length() > 0)
+								text.setText(text.getText().substring(0, text.getText().length() - 1));
+						} else if (BtnText.equalsIgnoreCase("(")) {
+							String txt = text.getText();
+							if (Calculation.isNumber(String.valueOf(txt.charAt(txt.length() - 1))))
+								text.append(' ' + BtnText + ' ');
+							else
+								text.append(BtnText + ' ');
+							// Operators
+						} else if (BtnText.equalsIgnoreCase(")")) {
+							text.append(' ' + BtnText);
+							// Operators
+						} else if (BtnText.equalsIgnoreCase("+")
+								|| BtnText.equalsIgnoreCase("-")
+								|| BtnText.equalsIgnoreCase("*")
+								|| BtnText.equalsIgnoreCase("/")
+								|| BtnText.equalsIgnoreCase("%")
+								|| BtnText.equalsIgnoreCase("^")
+								|| BtnText.equalsIgnoreCase("(")
+								|| BtnText.equalsIgnoreCase(")")
+								) {
+							text.append(' ' + BtnText + ' ');
+							// negative
+						} else if (BtnText.equalsIgnoreCase("(-)")) {
+							text.append("-");
+							// numbers
+						} else {
+							text.append(BtnText);
+						}
+					}
+				});
+				this.frame.add(temp.get(j));
+			}
+		}
+		this.frame.setVisible(true);
+	}
 }
